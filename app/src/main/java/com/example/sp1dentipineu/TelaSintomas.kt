dbb8fb391d18e65ac8442a9d9485c332ec321c07
rob <robson_appsantos@Hotmail.com>
2024-09-24 22:12:03 -0300

    Telas Mobile

---
+++ app/src/main/java/com/example/sp1dentipineu/TelaSintomas.kt
@@ -0,0 +1,21 @@
+package com.example.sp1dentipineu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaSintomas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_sintomas)
+
+
+
+
+
+    }
+
+}
